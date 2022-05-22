a74aa1dc105ede6feb83c44c3e60974c81775adf
Sam Brown <jumpbro@hotmail.com>
2022-05-22 10:19:41 +1000

    Organising and Commenting

--- js/main.js
+++ js/main.js
@@ -6,10 +6,3 @@ toggler.addEventListener('click', function(){
     this.classList.toggle('isActive')
     mobileMenu.classList.toggle('isActive')
 })
-
-// mobileNavs.forEach( link => {
-//     link.addEventListener('click', function(){
-//         mobileMenu.classList.toggle('isActive')
-//     })
-// })
-
